f62469083095c8496a4d56d4c9eb61c242b241c2
Saurabh Mittal <sdesaurabhmittal@gmail.com>
2023-04-29 16:33:17 +0530

    adding first version of react chat application

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const io = require('socket.io')(8000,{
+    cors: {
+      origin: "http://127.0.0.1:8080",
+      methods: ["GET", "POST"]
+    }
+  }); 
+
+const users = {};
+
+io.on("connection", socket =>{
+    // console.log("Socket object ", socket) ;
+    
+    socket.on("new-user-joined", name =>{
+        users[socket.id] = name ; 
+        console.log("new-user-joined : ", name);
+        socket.broadcast.emit("user-joined", name);
+    }); 
+
+    socket.on("receive", ({message, name})=>{
+        console.log(`user ${name} set message : ${message}`);
+        socket.broadcast.emit("receive", {'message': message, 'name' : name});
+    })
+    
+})
---
+++ js/client.js
@@ -0,0 +1,38 @@
+
+const socket = io('http://localhost:8000');
+
+let messageInput = document.getElementById("messageInp"); 
+let formElement = document.getElementById("inputForm"); 
+
+function append(message, position){
+    let messageContainer = document.getElementById("container"); 
+    let messageElement = document.createElement("div");
+    messageElement.innerHTML = message; 
+    messageElement.classList.add("message"); 
+    messageElement.classList.add(position); 
+
+    messageContainer.appendChild(messageElement);
+}
+
+const name = prompt("What is your name ? ", "anonymous"); 
+
+socket.emit("new-user-joined", name); 
+
+socket.on("user-joined", (name)=>{
+    append(`${name} joined the chat`, "left");
+})
+
+socket.on('receive', data =>{
+    let message = `${data.name} : ${data.message}`; 
+    append(message, "left"); 
+}); 
+
+formElement.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    let text = messageInput.value; 
+    append(`You : ${text}`, 'right'); 
+    socket.emit("receive", {'message': text, 'name' : name});
+    messageInput.value = "";
+})
+
+
